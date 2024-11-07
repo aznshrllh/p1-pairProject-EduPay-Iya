ab4e59793190d69d25b563b49204a380bc086751
aznshrllh <ahmadzakinashirullah@gmail.com>
2024-11-07 15:29:04 +0700

    test1

--- middlewares/isAuthenticated.js
+++
@@ -1,8 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-  if (!req.session.userId) {
-    return res.redirect("/login"); // Jika tidak ada session, arahkan ke login
-  }
-  next(); // Jika sudah login, lanjutkan ke controller berikutnya
-};
-
-module.exports = isAuthenticated;
